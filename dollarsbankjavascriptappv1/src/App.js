53313dc03fe9d4ff682dac98304714a49e8e431a
Noel <noel.cheng@rutgers.edu>
2020-08-06 23:21:28 -0400

    React setup

---
+++ dollarsbankjavascriptappv1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          DOLLARSBANK ATM Welcomes You!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
